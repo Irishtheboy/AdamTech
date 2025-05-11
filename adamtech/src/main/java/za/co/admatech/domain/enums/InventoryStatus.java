bdc53cc2c0d6a2b5514018833b4723c7cde1a2d7
Seymour732 <230185991@mycput.ac.za>
2025-05-11 12:11:53 +0200

    issues

---
+++ adamtech/src/main/java/za/co/admatech/domain/enums/InventoryStatus.java
@@ -0,0 +1,22 @@
+package za.co.admatech.domain.enums;
+
+public enum InventoryStatus {
+    IN_STOCK("In Stock"),
+    LOW_STOCK("Low Stock"),
+    OUT_OF_STOCK("Out of Stock");
+
+    private final String status;
+
+    InventoryStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return status;
+    }
+}
