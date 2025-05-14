b64db380506b5217a25a6b0cdab0b2fd933c2e39
LeGoober <rorisangmakganaiv@gmail.com>
2025-05-14 15:19:17 +0200

    Working on the Factory on the Customer domain and the Address, modified the POM.xml file to support SpringBoot testing

---
+++ adamtech/src/main/java/za/co/admatech/factory/CustomerFactory.java
@@ -0,0 +1,15 @@
+package za.co.admatech.factory;
+import za.co.admatech.domain.*;
+
+public class CustomerFactory {
+    public static Customer createCustomer(
+        String customerID,
+        String firstName,
+        String lastName,
+        String email,
+        Cart cardID,
+        Address address
+    ){
+        
+    }
+}
